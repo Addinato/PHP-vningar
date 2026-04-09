c2e0430a5748426975ec916641c19f9f7e48dcb6
Adrian Lydén <adrianlyden@gmail.com>
2026-04-09 09:54:50 +0200

    lite anteckningar

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body>
+        <h1>Form</h1>
+        <form action="test.php" method="GET">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+
+            <label for="Age">Age:</label>
+            <input type="number" id="Age" name="Age">
+
+            <input type="submit" value="Submit">
+        </form>
+
+        
+    </body>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,82 @@
+<?php
+    $name = $_GET['name'];
+    $age = $_GET['Age'];
+
+
+    $dagar = ["mån", "tis", "ons", "tors", "fre", "lör", "sön"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+            Name: <?php echo $name; ?>
+        </h1>
+
+        <h1>
+            Age: <?php echo $age; ?>
+        </h1>
+        
+        <h2>Sätt 1</h2>
+        <?php
+            if ($age <= 50) {
+                echo "<span>Du är fortfarande ung!</span>";
+            } else {
+                echo "<span>Du är fett gammal!</span>";
+            }
+        ?>
+
+        <h2>Sätt 2</h2>
+        <?php
+            if ($age <= 50) {
+        ?>
+            <span>Du är fortfarande ung!</span> 
+        <?php
+            }
+        
+            else {
+        ?>
+            <span>Du är fett gammal!</span>
+        <?php
+            }
+        ?>
+
+        <h2>Sätt 3</h2>
+
+        <span>
+            <?php
+                if ($age <= 50) {
+                    echo "Du är fortfarande ung!";
+                } else {
+                    echo "Du är fett gammal!";
+                }
+            ?>
+        </span>
+
+        <!-- Rekomenderat sätt att göra det på -->
+        <h2>Sätt 4</h2>
+
+        <?php
+        $message = "Du är fortfarande ung!";
+        if ($age >= 50) {
+            $message = "Du är fett gammal!";
+        }
+        ?>
+        <span><?php echo $message; ?></span>
+
+        <hr>
+        <select>
+            <?php
+                foreach ($dagar as $dag) {
+                    echo "<option value='$dag'>$dag</option>";
+                }
+            ?>
+        </select>
+
+    </body>
+</html>
